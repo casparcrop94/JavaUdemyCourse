1a42a2d8d6cc6ae75990a10b9d25f2488e1f8c01
casparcrop94 <casparcrop@gmail.com>
2023-03-22 15:36:43 +0100

    Section 2-4 finished

--- workbooks/Module 1 - Introduction to Java/2. Variables/Workbook 2.5/SignUp.java
+++ workbooks/Module 1 - Introduction to Java/2. Variables/Workbook 2.5/SignUp.java
@@ -11,26 +11,32 @@ public class SignUp {
         System.out.println("Welcome to JavaGram! Let's sign you up.");
         
         System.out.println("What is your first name?");
+        String firstName = scan.nextLine();
         
         System.out.println("What is your last name?");
+        String lastName = scan.nextLine();
 
         System.out.println("How old are you?");
+        int age = scan.nextInt();
 
         System.out.println("Make a username");  
-        // If you experience trouble picking up the username, please carefully read the instructions on Learn the Part.
-        
+        scan.nextLine(); //Throwaway nextLine to get around the trap
+        String userName = scan.nextLine();
+
         System.out.println("What city do you live in?");
+        String placeOfResidence = scan.nextLine();
 
         System.out.println("What country is that?");
+        String countryOfResidence = scan.nextLine();
         
         System.out.println("Thank you for joining JavaGram!");
         System.out.println("\nHere is the information you entered:");
-        System.out.println("\tFirst Name: <firstName>");
-        System.out.println("\tLast Name: <lastName>");
-        System.out.println("\tAge: <age>");
-        System.out.println("\tUsername: <userName>");
-        System.out.println("\tCity: <city>");
-        System.out.println("\tCountry: <country>");
+        System.out.println("\tFirst Name: "+firstName);
+        System.out.println("\tLast Name: "+lastName);
+        System.out.println("\tAge: "+age);
+        System.out.println("\tUsername: "+userName);
+        System.out.println("\tCity: "+placeOfResidence);
+        System.out.println("\tCountry: "+countryOfResidence);
 
         //close scanner. It's good practice :D ! 
     }
